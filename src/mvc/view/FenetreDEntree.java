f036fdcda630074fd367cbd357801b4b8e01c5ac
ConcepDevAnge <Anglique MACHU@LAPTOP-C0GOEE9C.home>
2019-02-24 22:25:24 +0100

    _ Tableau des contacts pour le 1er onglet réalisé avec colonnes correctement dimensionnées et titrées _ Placement d'un ascenseur (scrollbar) dans le tableau des contacts, bien à droite.

--- src/mvc/view/FenetreDEntree.java
+++ src/mvc/view/FenetreDEntree.java
@@ -16,11 +16,20 @@ import javax.swing.JTabbedPane;
 import java.awt.FlowLayout;
 import javax.swing.SwingConstants;
 import javax.swing.Box;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Font;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class FenetreDEntree {
 
 	private JFrame frmGestionDesContacts;
 	private FenetreDEntreeModel model;
+	private JTable table;
 
 	/**
 	 * Create the application.
@@ -61,6 +70,55 @@ public class FenetreDEntree {
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Liste des contacts", null, panel_1, null);
+		
+		table = new JTable();
+		table.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 11));
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"Pseudonyme", "\u2640 \u2642", "Nom", "Pr\u00E9nom", "Adresse", "Code Postal", "Ville"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, Object.class, String.class, String.class, String.class, Integer.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(92);
+		table.getColumnModel().getColumn(1).setPreferredWidth(30);
+		table.getColumnModel().getColumn(2).setPreferredWidth(92);
+		table.getColumnModel().getColumn(3).setPreferredWidth(92);
+		table.getColumnModel().getColumn(4).setPreferredWidth(205);
+		table.getColumnModel().getColumn(5).setPreferredWidth(67);
+		table.getColumnModel().getColumn(6).setPreferredWidth(112);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+		JScrollPane scrollPane = new JScrollPane(table);
+		panel_1.add(scrollPane);
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("Rechercher", null, panel_2, null);
